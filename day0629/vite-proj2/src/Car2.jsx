b321ce89c5c05f7eb01e2948cb9a71c7cb2f432e
LSJ <1007ckddjs12@gmail.com>
2026-06-29 17:21:58 +0900

    feat: new file

--- day0629/vite-proj2/src/Car2.jsx
+++ day0629/vite-proj2/src/Car2.jsx
@@ -1,7 +1,7 @@
 function Car2(props) {
   return (
     <div>
-      <h2>
+      <h2 className={props.carinfo.color}>
         내 차는 {props.carinfo.name} {props.carinfo.model}!<br />
         {props.carinfo.color}에 {props.carinfo.year}년식이야.
       </h2>
--- day0629/vite-proj2/src/Garage.jsx
+++ day0629/vite-proj2/src/Garage.jsx
@@ -5,7 +5,28 @@ const mycar1 = {
   name: "Ford",
   model: "Mustang",
   color: "red",
-  year: 1969,
+  year: 2015,
+};
+
+const mycar2 = {
+  name: "람보르기니",
+  model: "우라칸",
+  color: "yellow",
+  year: 2016,
+};
+
+const mycar3 = {
+  name: "제네시스",
+  model: "GV90",
+  color: "black",
+  year: 2024,
+};
+
+const mycar4 = {
+  name: "BMW",
+  model: "M3",
+  color: "blue",
+  year: 2020,
 };
 
 function Garage() {
@@ -16,6 +37,9 @@ function Garage() {
       <Car brand="르노" />
       <Car brand="기아" />
       <Car2 carinfo={mycar1} />
+      <Car2 carinfo={mycar2} />
+      <Car2 carinfo={mycar3} />
+      <Car2 carinfo={mycar4} />
     </div>
   );
 }
