d4600eb71bdd2b4c0ad6eadb826f2048d35c9616
LSJ <1007ckddjs12@gmail.com>
2026-06-30 10:54:13 +0900

    feat: add useState.jsx

--- day0630/src/main.jsx
+++ day0630/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import Header from "./sementic/Header";
 import Footer from "./sementic/Footer";
 import "./main.css";
+import MyUseState from "./sementic/MyUseState";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
+    <MyUseState />
     <Footer />
   </StrictMode>,
 );
