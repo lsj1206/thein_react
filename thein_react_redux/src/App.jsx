b1bb1334502afa29af3ef9170b5a42c189fdb8ed
LSJ <1007ckddjs12@gmail.com>
2026-07-02 13:44:56 +0900

    feat: install react-router

--- thein_react_redux/src/App.jsx
+++ thein_react_redux/src/App.jsx
@@ -1,49 +1,30 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { increase, decrease } from "./counterSlice";
-import { useState } from "react";
-import { addTodo, deleteTodo } from "./todoSlice";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
 
 function App() {
-  // -------- 카운터---상태 읽기
-  const count = useSelector((state) => state.counter.value);
-  // -------- 카운터---상태 변경 요청
-  const dispatch = useDispatch();
-
-  // --- 할일 목록 --- 상태 관리
-  const [text, setText] = useState("");
-  // --- 할일 목록 --- 상태 읽기
-  const todos = useSelector((state) => state.todo.list);
-  // --- 할일 목록 --- store 변경 요청
-  const todoDispatch = useDispatch();
-  // --- 할일 목록 --- 목록추가 함수
-  const handleAdd = () => {
-    if (text === "") return;
-    todoDispatch(addTodo(text));
-    setText("");
-  };
-
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increase())}>더하기</button>
-      <button onClick={() => dispatch(decrease())}>빼기</button>
-      <hr />
-
-      <h1>할 일 목록</h1>
-      <p>
-        목록입력: <input value={text} onChange={(e) => setText(e.target.value)} autoFocus />
-      </p>
-      <br />
-      <button type="text" onClick={handleAdd}>
-        추가
-      </button>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          {todo.text}
-          <button onClick={() => todoDispatch(deleteTodo(todo.id))}>삭제</button>
-        </div>
-      ))}
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+      </nav>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
--- thein_react_redux/src/main.jsx
+++ thein_react_redux/src/main.jsx
@@ -3,10 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   // 전체 앱에서 Redux 사용 가능하게 설정
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
