58cfbb264993897742f4d1300639e161eabbcf05
LSJ <1007ckddjs12@gmail.com>
2026-07-02 11:57:08 +0900

    feat: new redux example

--- thein_react_redux/src/main.jsx
+++ thein_react_redux/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  // 전체 앱에서 Redux 사용 가능하게 설정
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>,
+);
